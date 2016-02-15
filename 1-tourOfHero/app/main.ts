3c343d8ac5f1d884869a4f151e737f8d895abee3
Jarek <klata.jarek@gmail.com>
2016-02-15 12:59:40 +0000

    start tour of Hero

---
+++ 1-tourOfHero/app/main.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/platform/browser';
+// When another module needs to refer to AppComponent, it imports the AppComponent
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
